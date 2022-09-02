8a5b1332fbc718d102de386e8a47adfdb854d030
ASWATHI23 <110898037+ASWATHI23@users.noreply.github.com>
2022-09-02 15:08:31 +0530

    second.commit

---
+++ create_database.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+
+conn = mysql.connector.connect(user='root',password='abc123',host='127.0.0.1')
+
+
+cursor = conn.cursor()
+cursor.execute("DROP database IF EXISTS MyDatabase")
+
+sql = "CREATE database MYDATABASE"
+
+cursor.execute(sql)
+
+print("List of database:")
+cursor.execute("SHOW DATABASES")
+print(cursor.fetchall())
+
+conn.close()
+
+
